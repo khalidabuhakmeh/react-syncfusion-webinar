7b8eded82fc7f99cae2611ed9557bbea6d77a07e
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-08-25 16:09:25 -0400

    Add Prettier

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, {useContext} from 'react';
+import React, { useContext } from "react";
 import "./App.css";
-import {FruitContext} from './Contexts/FruitContext';
+import { FruitContext } from "./Contexts/FruitContext";
 
 export function App() {
-    const fruits = useContext(FruitContext);
-    return (
-        <>
-            <h1>Fruit Basket ({fruits.length})</h1>
-            <ul>
-                {fruits.map(fruit => (
-                    <li key={fruit.name}>
-                        {fruit.name} (${fruit.price.toFixed(2)})
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+  const fruits = useContext(FruitContext);
+  return (
+    <>
+      <h1>Fruit Basket ({fruits.length})</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.name}>
+            {fruit.name} (${fruit.price.toFixed(2)})
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './App';
-import {FruitContext} from "./Contexts/FruitContext";
-import {fruits} from "./Data/Fruits";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import { FruitContext } from "./Contexts/FruitContext";
+import { fruits } from "./Data/Fruits";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <FruitContext.Provider value={fruits}>
-            <App/>
-        </FruitContext.Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <FruitContext.Provider value={fruits}>
+      <App />
+    </FruitContext.Provider>
+  </React.StrictMode>,
 );
