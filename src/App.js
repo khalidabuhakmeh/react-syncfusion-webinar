9232da39853ea8bc072d4e8c645d6f7554545348
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2023-09-08 12:56:04 -0400

    Update code with the latest changes from Chad.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
 import React, { useContext } from "react";
+import { ListViewComponent } from "@syncfusion/ej2-react-lists";
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  GridComponent,
+} from "@syncfusion/ej2-react-grids";
+import {
+  Category,
+  ChartComponent,
+  ColumnSeries,
+  Inject,
+  LineSeries,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
 import "./App.css";
 import { FruitContext } from "./Contexts/FruitContext";
 
@@ -6,14 +22,37 @@ export function App() {
   const fruits = useContext(FruitContext);
   return (
     <>
-      <h1>Fruit Basket ({fruits.length})</h1>
-      <ul>
-        {fruits.map((fruit) => (
-          <li key={fruit.name}>
-            {fruit.name} (${fruit.price.toFixed(2)})
-          </li>
-        ))}
-      </ul>
+      <div className={"container"}>
+        <h2>List component</h2>
+        <ListViewComponent
+          id="list"
+          dataSource={fruits}
+          fields={{ text: "name" }}
+        />
+      </div>
+      <div className={"container"}>
+        <h2>Grid component</h2>
+        <GridComponent id="grid" dataSource={fruits}>
+          <ColumnsDirective>
+            <ColumnDirective field="name" headerText="Name" width="100" />
+            <ColumnDirective field="price" headerText="Price" width="100" />
+          </ColumnsDirective>
+        </GridComponent>
+      </div>
+      <div className={"container"}>
+        <h2>Chart component</h2>
+        <ChartComponent id="charts" primaryXAxis={{ valueType: "Category" }}>
+          <Inject services={[ColumnSeries, Tooltip, LineSeries, Category]} />
+          <SeriesCollectionDirective>
+            <SeriesDirective
+              dataSource={fruits}
+              xName="name"
+              yName="price"
+              name="Fruits"
+            />
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
     </>
   );
 }
